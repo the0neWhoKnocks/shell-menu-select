134aa8465989ae5a5b2e0bffb57aed4e34c044fb
Nox <the0neWhoKnocks@users.noreply.github.com>
2021-10-10 21:03:24 -0700

    refactor: Flags, indicator, and color
    - Added argument parsing which now allows for flags.
      - Moved what used to be in the `example.sh` file, into the output for 
    `--help`.
    - Changed the selection indicator to an arrow since the previous one 
    didn't work well on all shells.
    - The selection now hightlights in green.
    - Added doc on how to remotely import the script.

--- README.md
+++ README.md
@@ -1,17 +1,28 @@
 # shell-menu-select
 
-A script that allows a user to use arrow keys to select an item from a list.
-Sometimes using `select` can be a bit clunky because the user doesn't get any
-visual feedback about what they're selecting unless a confirmation message is
-displayed (which adds more overhead).
+A script that allows a user to use arrow keys to select an item from a list. Sometimes using `select` can be a bit clunky because the user doesn't get any visual feedback about what they're selecting unless a confirmation message is displayed (which adds more overhead).
+
+---
+
+## How to load
+
+Typical (you have the script locally)
+```sh
+source ./get-choice.sh
+```
+
+Remote (you want the functionality, but don't want to clone or copy anything)
+```sh
+source <(wget -qO- https://raw.githubusercontent.com/the0neWhoKnocks/shell-menu-select/master/get-choice.sh)
+```
 
 ---
 
 ## How to use
 
-An example of how to use the script can be found in `example.sh`. You can run
-the code below to see it in action.
+All flags and an example are demonstrated via the `--help` command.
 
-```
-./example.sh
+```sh
+source ./get-choice.sh
+getChoice -h
 ```
---
+++ get-choice.sh
@@ -0,0 +1,185 @@
+#!/bin/bash
+
+CHAR__GREEN='\033[0;32m'
+CHAR__RESET='\033[0m'
+menuStr=""
+
+function hideCursor(){
+  printf "\033[?25l"
+
+  # capture CTRL+C so cursor can be reset
+  trap "showCursor && exit 0" 2
+}
+
+function showCursor(){
+  printf "\033[?25h"
+}
+
+function clearLastMenu(){
+  local msgLineCount=$(printf "$menuStr" | wc -l)
+  # moves the curser up N lines so the output overwrites it
+  echo -en "\033[${msgLineCount}A"
+
+  # clear to end of screen to ensure there's no text left behind from previous input
+  [ $1 ] && tput ed
+}
+
+function renderMenu(){
+  local start=0
+  local selector=""
+  local instruction="$1"
+  local selectedIndex=$2
+  local listLength=$itemsLength
+  local longest=0
+  local spaces=""
+  menuStr="\n $instruction\n"
+
+  # Get the longest item from the list so that we know how many spaces to add
+  # to ensure there's no overlap from longer items when a list is scrolling up or down.
+  for (( i=0; i<$itemsLength; i++ )); do
+    if (( ${#menuItems[i]} > longest )); then
+      longest=${#menuItems[i]}
+    fi
+  done
+  spaces=$(printf ' %.0s' $(eval "echo {1.."$(($longest))"}"))
+
+  if [ $3 -ne 0 ]; then
+    listLength=$3
+
+    if [ $selectedIndex -ge $listLength ]; then
+      start=$(($selectedIndex+1-$listLength))
+      listLength=$(($selectedIndex+1))
+    fi
+  fi
+
+  for (( i=$start; i<$listLength; i++ )); do
+    local currItem="${menuItems[i]}"
+    currItemLength=${#currItem}
+
+    if [[ $i = $selectedIndex ]]; then
+      selectedChoice="${currItem}"
+      selector="${CHAR__GREEN}ᐅ${CHAR__RESET}"
+      currItem="${CHAR__GREEN}${currItem}${CHAR__RESET}"
+    else
+      selector=" "
+    fi
+
+    currItem="${spaces:0:0}${currItem}${spaces:currItemLength}"
+
+    menuStr="${menuStr}\n ${selector} ${currItem}"
+  done
+
+  menuStr="${menuStr}\n"
+
+  # whether or not to overwrite the previous menu output
+  [ $4 ] && clearLastMenu
+
+  printf "${menuStr}"
+}
+
+function getChoice(){
+  local KEY__ARROW_UP=$(echo -e "\033[A")
+  local KEY__ARROW_DOWN=$(echo -e "\033[B")
+  local KEY__ENTER=$(echo -e "\n")
+  local captureInput=true
+  local displayHelp=false
+  local maxViewable=0
+  local instruction="Select an item from the list:"
+  local selectedIndex=0
+
+  remainingArgs=()
+  while [[ $# -gt 0 ]]; do
+    key="$1"
+
+    case $key in
+      -h|--help)
+        displayHelp=true
+        shift
+        ;;
+      -i|--index)
+        selectedIndex=$2
+        shift 2
+        ;;
+      -m|--max)
+        maxViewable=$2
+        shift 2
+        ;;
+      -o|--options)
+        menuItems=$2[@]
+        menuItems=("${!menuItems}")
+        shift 2
+        ;;
+      -q|--query)
+        instruction="$2"
+        shift 2
+        ;;
+      *)
+        remainingArgs+=("$1")
+        shift
+        ;;
+    esac
+  done
+
+  # just display help
+  if $displayHelp; then
+    echo;
+    echo "Usage: getChoice [OPTION]..."
+    echo "Renders a keyboard navigable menu with a visual indicator of what's selected."
+    echo;
+    echo "  -h, --help     Displays this message"
+    echo "  -i, --index    The initially selected index for the options"
+    echo "  -m, --max      Limit how many options are displayed"
+    echo "  -o, --options  An Array of options for a User to choose from"
+    echo "  -q, --query    Question or statement presented to the User"
+    echo;
+    echo "Example:"
+    echo "  foodOptions=(\"pizza\" \"burgers\" \"chinese\" \"sushi\" \"thai\" \"italian\" \"shit\")"
+    echo;
+    echo "  getChoice -q \"What do you feel like eating?\" -o foodOptions -i \$((\${#foodOptions[@]}-1)) -m 4"
+    echo "  printf \"\\n First choice is '\${selectedChoice}'\\n\""
+    echo;
+    echo "  getChoice -q \"Select another option in case the first isn't available\" -o foodOptions"
+    echo "  printf \"\\n Second choice is '\${selectedChoice}'\\n\""
+    echo;
+
+    return 0
+  fi
+
+  set -- "${remainingArgs[@]}"
+  local itemsLength=${#menuItems[@]}
+
+  # no menu items, at least 1 required
+  if [[ $itemsLength -lt 1 ]]; then
+    printf "\n [ERROR] No menu items provided\n"
+    exit 1
+  fi
+
+  renderMenu "$instruction" $selectedIndex $maxViewable
+  hideCursor
+
+  while $captureInput; do
+    read -rsn3 key # `3` captures the escape (\033'), bracket ([), & type (A) characters.
+
+    case "$key" in
+      "$KEY__ARROW_UP")
+        selectedIndex=$((selectedIndex-1))
+        (( $selectedIndex < 0 )) && selectedIndex=$((itemsLength-1))
+
+        renderMenu "$instruction" $selectedIndex $maxViewable true
+        ;;
+
+      "$KEY__ARROW_DOWN")
+        selectedIndex=$((selectedIndex+1))
+        (( $selectedIndex == $itemsLength )) && selectedIndex=0
+
+        renderMenu "$instruction" $selectedIndex $maxViewable true
+        ;;
+
+      "$KEY__ENTER")
+        clearLastMenu true
+        showCursor
+        captureInput=false
+        ;;
+    esac
+  done
+}
